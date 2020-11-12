6760217f925c3d23806c791e6bb901a283a5edd1
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-11-12 10:47:10 +0100

    Add files via upload

---
+++ sinozicSebastianPonavljanje2/sinozicSebastianPonavljanje2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianPonavljanje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abc = "S S S S S";
+            abc = abc.Replace(" ", "_");
+
+            Console.WriteLine(abc);
+            Console.ReadKey();
+        }
+        
+               
+
+}
+}
